f3ca02449507e32e76c1a983f7068d013e10355b
ThanhNghiaNg <nghiant089@gmail.com>
2023-05-15 16:11:41 +0700

    update for deploy

--- app.js
+++ app.js
@@ -14,60 +14,68 @@ const Transaction = require("./models/Transaction");
 
 require("dotenv").config();
 
-console.log(process.env.MONGO_URI);
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const FE_CLIENT_URL = process.env.FE_CLIENT_URL;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const PRODUCTION = process.env.PRODUCTION;
 
 const store = new MongoDBStore({
-  uri: process.env.MONGO_URI,
+  uri: MONGO_URI,
   collection: "UserSessions",
 });
 
 const app = express();
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-// DEVELOP
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
-
-app.use(
-  session({
-    secret: "SESSION_SECRET",
-    saveUninitialized: false,
-    resave: false,
-    store: store,
-    cookie: {
-      // sameSite: "none", // UNCOMMENT FOR DEPLOY
-      // secure: true, // UNCOMMENT FOR DEPLOY
-      maxAge: 10000 * 60 * 60 * 24, // One day in milliseconds
-    },
-  })
-);
-
-// // DEPLOY
+// // DEVELOP
 // app.use(
 //   cors({
-//     origin: [],
+//     origin: true,
 //     credentials: true,
-//     method: ["POST, PUT, PATCH, DELETE, GET"]
 //   })
 // );
 
 // app.use(
 //   session({
-//     secret: "my secret",
-//     resave: false,
+//     secret: SESSION_SECRET,
 //     saveUninitialized: false,
+//     resave: false,
 //     store: store,
 //     cookie: {
-//       sameSite: "none",
-//       secure: true,
-//       maxAge: 1000 * 3600 * 24 * 30, // 1 month
+//       // sameSite: "none", // UNCOMMENT FOR DEPLOY
+//       // secure: true, // UNCOMMENT FOR DEPLOY
+//       maxAge: 10000 * 60 * 60 * 24, // One day in milliseconds
 //     },
 //   })
 // );
+
+// DEPLOY
+app.use(
+  cors({
+    origin: PRODUCTION ? [FE_CLIENT_URL] : true,
+    credentials: true,
+    method: ["POST, PUT, PATCH, DELETE, GET"],
+  })
+);
+
+console.log({
+  maxAge: 10000 * 3600 * 24 * 30, // 10 day
+  ...(PRODUCTION ? { sameSite: "none", secure: true } : {}),
+});
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      maxAge: 10000 * 3600 * 24 * 30, // 10 day
+      ...(PRODUCTION ? { sameSite: "none", secure: true } : {}),
+    },
+  })
+);
 
 app.use(async (req, res, next) => {
   if (!req.session.user) {
@@ -93,7 +101,7 @@ app.use("/category", categoryRoutes);
 app.use("/transaction", transactionRoutes);
 app.use("/partner", partnerRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(process.env.PORT || 5000);
+mongoose.connect(MONGO_URI).then(() => {
+  app.listen(PORT || 5000);
   console.log("Server is running...");
 });
