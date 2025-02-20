0ec1ba9421bcd4b2a88f2a5c960b667d558378f5
Thanh Nghia <nghiant089@gmail.com>
2025-02-20 21:22:10 +0700

    for easy to test

--- routes/cron.js
+++ routes/cron.js
@@ -2,6 +2,6 @@ const cronControllers = require("../controllers/cron");
 const express = require("express");
 const router = express.Router();
 
-router.post("/update-most-used-category", cronControllers.updateMostUsedCategoryAndType);
+router.get("/update-most-used-category", cronControllers.updateMostUsedCategoryAndType);
 
 module.exports = router;
