eb00ab21e4475c437ab818084a4ddbe6471d2f0b
Thanh Nghia <nghiant089@gmail.com>
2025-08-27 09:39:44 +0700

    update router function call

--- routes/push-noti.js
+++ routes/push-noti.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get("/", pushNotiControllers.pushNoti);
 
-router.postSubscribe("/subscribe", pushNotiControllers.pushNoti);
+router.post("/subscribe", pushNotiControllers.pushNoti);
 
-router.postUnSubscribe("/unsubscribe", pushNotiControllers.pushNoti);
+router.post("/unsubscribe", pushNotiControllers.pushNoti);
 
 module.exports = router;
