24a6737b2d8c8439bdbd0cbbc0100586d0438c77
Nigel <nghiant089@gmail.com>
2026-05-04 15:50:15 +0000

    feat: add profile model foundation

---
+++ models/Profile.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const profileSchema = Schema(
+  {
+    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    name: { type: String, required: true },
+    avatarUrl: { type: String, required: false },
+    color: { type: String, required: false },
+    isDefault: { type: Boolean, default: false },
+    order: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+profileSchema.index({ user: 1, order: 1 }, { background: true });
+profileSchema.index({ user: 1, isDefault: 1 }, { background: true });
+
+module.exports = mongoose.model("Profile", profileSchema);
